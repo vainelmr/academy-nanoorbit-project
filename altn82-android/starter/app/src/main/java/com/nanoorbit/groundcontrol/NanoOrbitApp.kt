b8f245b88b2ecb626698c457dd626591fa91ff09
Hadis010 <hafsadini001@gmail.com>
2026-04-29 11:13:28 +0200

    (layout) Wire NanoOrbitTheme with navigation container

--- altn82-android/starter/app/src/main/java/com/nanoorbit/groundcontrol/NanoOrbitApp.kt
+++ altn82-android/starter/app/src/main/java/com/nanoorbit/groundcontrol/NanoOrbitApp.kt
@@ -1,17 +1,12 @@
 package com.nanoorbit.groundcontrol
 
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import com.nanoorbit.groundcontrol.ui.screens.dashboard.DashboardScreen
+import com.nanoorbit.groundcontrol.navigation.NanoOrbitNavigation
 import com.nanoorbit.groundcontrol.ui.theme.NanoOrbitTheme
 
 @Composable
 fun NanoOrbitApp() {
     NanoOrbitTheme {
-        Scaffold { innerPadding ->
-            DashboardScreen(modifier = Modifier.padding(innerPadding))
-        }
+        NanoOrbitNavigation()
     }
 }
