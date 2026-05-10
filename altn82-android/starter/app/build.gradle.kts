9898d866cba3e8a43bc77fc61e98bd572d041045
Hadis010 <hafsadini001@gmail.com>
2026-05-10 17:21:02 +0200

    (deps) Add DataStore and Compose material dependencies

--- altn82-android/starter/app/build.gradle.kts
+++ altn82-android/starter/app/build.gradle.kts
@@ -67,6 +67,8 @@ dependencies {
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material:material")
+    implementation("androidx.compose.material:material-icons-extended")
     implementation("com.google.android.material:material:1.12.0")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
@@ -85,6 +87,7 @@ dependencies {
 
     implementation("org.osmdroid:osmdroid-android:6.1.18")
     implementation("androidx.work:work-runtime-ktx:2.9.1")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
